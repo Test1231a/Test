f5f05bfab3da42f060e0d20206e07e8240094096
xiulankaixin <602813011@qq.com>
2016-07-21 18:29:46 +0800

    add test 5 and delete some sssss

--- TestWEB/src/TestMain.java
+++ TestWEB/src/TestMain.java
@@ -7,12 +7,8 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("modify 3 times");
-		System.out.println("This is message");
-		double l = Math.PI;
-		System.currentTimeMillis();
-		System.out.println("modify 4 time");
-		System.out.println("Test 4");
+		
+		System.out.println("Test 5");
 
 	}
 
