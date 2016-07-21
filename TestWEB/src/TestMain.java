9114110bb8dc44b371a7e698c618859eadc129af
yandongpeng <ghostydp@vip.qq.com>
2016-07-21 17:55:02 +0800

    add test 3

--- TestWEB/src/TestMain.java
+++ TestWEB/src/TestMain.java
@@ -9,7 +9,7 @@ public class TestMain {
 		System.out.println("modify 3 times");
 		System.out.println("This is message");
 		double l = Math.PI;
-		System.out.println("Test 1");
+		System.out.println("Test 3");
 	}
 
 }
