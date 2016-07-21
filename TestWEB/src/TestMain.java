1a215b468b455c1f35e3b5799546f8f87d281961
blackwindrunner <yandongpeng@qq.com>
2016-07-21 17:31:48 +0800

    add message "This is message"

--- TestWEB/src/TestMain.java
+++ TestWEB/src/TestMain.java
@@ -7,6 +7,7 @@ public class TestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("modify 3 times");
+		System.out.println("This is message");
 	}
 
 }
